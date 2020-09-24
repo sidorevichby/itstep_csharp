27e868a42d56a6d8ba461faee99826128ea4a890
sidorevichby <33716355+sidorevichby@users.noreply.github.com>
2020-09-24 16:17:50 +0300

    Range

---
+++ Lecture_1/Homework_3_Range/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework_3_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber, secondnumber;
+            firstnumber = Convert.ToInt32(Console.ReadLine());
+            secondnumber = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                for (int i = 0; i < firstnumber; i++)
+                {
+                    Console.Write("{0} ", firstnumber);
+                }
+                Console.WriteLine();
+                firstnumber++;
+            }
+            while (firstnumber <= secondnumber);
+        }
+    }
+}
