b9751406dcd8c4aa23293d1a2eee033285a6aef7
sidorevichby <33716355+sidorevichby@users.noreply.github.com>
2020-09-24 17:05:04 +0300

    Init RightToLeft

---
+++ Lecture_1/Homework_4_RightToLeft/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework_4_RightToLeft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int length, output, inputInt;
+            input = Console.ReadLine();
+            length = input.Length;
+            inputInt = Convert.ToInt32(input);
+            while (length>0)
+            {
+                output = inputInt % 10;
+                inputInt /= 10;
+                length--;
+                Console.Write(output);
+            }
+            Console.WriteLine();
+        }
+    }
+}
