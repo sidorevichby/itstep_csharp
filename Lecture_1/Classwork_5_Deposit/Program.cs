3f286d2f662ace81397c51e1e9c3b71d2ba114c5
sidorevichby <33716355+sidorevichby@users.noreply.github.com>
2020-09-24 13:05:21 +0300

    deposit

---
+++ Lecture_1/Classwork_5_Deposit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classwork_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = 1000, finishdeposit = 1100;
+            double P;
+            int monthcounter = 0;
+            P = Convert.ToDouble(Console.ReadLine());
+            do
+            {
+                deposit = deposit + (deposit * P / 100);
+                monthcounter++;
+                Console.WriteLine(deposit);
+            } while (deposit < finishdeposit);
+            Console.WriteLine("Finish deposit {0} at percent = {1}. Month = {2}", deposit, P, monthcounter);
+        }
+    }
+}
