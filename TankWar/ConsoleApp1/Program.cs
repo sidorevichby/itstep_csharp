426b49f5e8997cf53d2589d5091f850e5b1a8cc5
sidorevichby <33716355+sidorevichby@users.noreply.github.com>
2020-10-25 13:31:52 +0300

    add List
    add List

--- TankWar/ConsoleApp1/Program.cs
+++ TankWar/ConsoleApp1/Program.cs
@@ -12,20 +12,30 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Random rnd = new Random();
+            List<string> Wins = new List<string>();
             int warresult = 0;
             bool result;
-            for (int i = 0; i < 5; i++)
+
+            for (int i = 1; i < 6; i++)
             {
                 T34 t1 = new T34 { };
                 Pantera t2 = new Pantera { };
-
                 result = t1 ^ t2;
-                //t1.parameters();
-                //t2.parameters();
-                if (result)
-                    warresult++;
-                Console.WriteLine($"First tank win? {result}");
 
+                if (result)
+                {
+                    Wins.Add(t1.model);
+                    warresult++;
+                }
+                else
+                    Wins.Add(t2.model);
+
+            }
+
+            Wins.Sort();
+            foreach (string i in Wins)
+            {
+                Console.WriteLine(i);
             }
             Console.WriteLine();
             if (warresult>=3)
