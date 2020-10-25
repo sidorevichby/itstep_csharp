78aae4acef2152554df0f4748ce18bef1bc394af
sidorevichby <33716355+sidorevichby@users.noreply.github.com>
2020-10-25 12:29:34 +0300

    Revert "clean"
    This reverts commit 992df5623dd00fcd01f10f037064f78a33f696fe.

---
+++ Lecture_1/ClassLibrary1/Pantera.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Pantera :Tank
+    {
+        Random rnd = new Random();
+
+        public Pantera(string model = "Pantera") : base(model)
+        {
+            this.ammunitions = rnd.Next(0, 100);
+            this.armor = rnd.Next(0, 100);
+            this.mobility = rnd.Next(0, 100);
+        }
+        public void parameters()
+        {
+            Console.WriteLine($"{model}, ammu={ammunitions}, armo = {armor}, mobi = {mobility}");
+        }
+    }
+}
