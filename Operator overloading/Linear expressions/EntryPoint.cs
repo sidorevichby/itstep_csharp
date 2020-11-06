1134d906689022f905d39e86c82af1a112b9460a
sidorevichby <33716355+sidorevichby@users.noreply.github.com>
2020-11-06 15:49:33 +0300

    createTask_LinearExpressions

---
+++ Operator overloading/Linear expressions/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+using LinearLib;
+
+namespace Linear_expressions
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            linearExpressions one = new linearExpressions();
+            one.Get();
+            one.Print();
+            linearExpressions two = new linearExpressions();
+            two.Get();
+            two.Print();
+            result= one + two;
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ Operator overloading/LinearLib/linearExpressions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace LinearLib
+{
+    public class linearExpressions
+    {
+        public int a;
+        public int b;
+        public static string str = "";
+
+        public void Get()
+        {
+            str = Console.ReadLine();
+            Parse();
+        }
+
+
+        private void Parse()
+        {
+
+                string a_str = "", b_str = "";
+                bool key = true;
+
+                foreach (var i in str)
+                {
+
+                    if (i != ',' && i != ' ')
+                {
+
+                        if (key == true)
+                        {
+                            a_str += Convert.ToString(i);
+                        }
+                        else
+                        {
+                            b_str += Convert.ToString(i);
+                        }
+
+                    }
+
+                    else
+
+                    {
+                        key = false;
+                    }
+               
+                }
+            a = Convert.ToInt32(a_str);
+            b = Convert.ToInt32(b_str);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{a} * X + {b} * Y = 0");
+        }
+
+        public static int operator +(linearExpressions fistLine, linearExpressions secondLine)
+        {
+            int delta, delta1, delta2;
+            delta = fistLine.a * secondLine.b - secondLine.a * fistLine.b;
+            return delta;
+
+        }
+    }
+}
