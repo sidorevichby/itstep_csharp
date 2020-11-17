590b849d5891b60ea48a4844b7e6be588e6d4f01
sidorevichby <33716355+sidorevichby@users.noreply.github.com>
2020-11-17 23:55:01 +0300

    Create Selenium Google Test
    1 Create folder Selenium
    2 Create project GoogleTest

---
+++ Selenium/GoogleTest/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleTest
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test case start");
+            IWebDriver brouser = new ChromeDriver();
+            brouser.Navigate().GoToUrl("http://www.google.com");
+            Thread.Sleep(2000);
+            IWebElement search = brouser.FindElement(By.Name("q"));
+            search.SendKeys("selenium tutorials");
+            Thread.Sleep(2000);
+            IWebElement searchButton = brouser.FindElement(By.Name("btnK"));
+            searchButton.Click();
+            Thread.Sleep(2000);
+            brouser.Close();
+            Console.WriteLine("Test case ended");
+        }
+    }
+}
